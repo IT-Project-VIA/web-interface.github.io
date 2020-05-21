e1fb2ba67c1dc02d67b27cfcc823d92fe3697cc5
Tommus1997 <tom.griebe@gmail.com>
2020-05-21 16:56:31 +0200

    Updates

--- precache-manifest.0749baff1c6a9a7e9f33c475058c3726.js
+++ precache-manifest.0749baff1c6a9a7e9f33c475058c3726.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "87ef1f0ff384d4e5d7ef1ca6c5d4bb7f",
+    "revision": "cf6f26d4665aafde051e94f42ea4c19c",
     "url": "/web-interface.github.io/index.html"
   },
   {
-    "revision": "bc37eedeb2f59d2b9fe5",
+    "revision": "6087a4641ccdeec276dc",
     "url": "/web-interface.github.io/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "732c9d8deb589d38e0da",
-    "url": "/web-interface.github.io/static/js/2.b538c845.chunk.js"
+    "revision": "fdd28dba918fd2dea9cf",
+    "url": "/web-interface.github.io/static/js/2.77ce5a91.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/web-interface.github.io/static/js/2.b538c845.chunk.js.LICENSE.txt"
+    "url": "/web-interface.github.io/static/js/2.77ce5a91.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bc37eedeb2f59d2b9fe5",
-    "url": "/web-interface.github.io/static/js/main.f28a98e1.chunk.js"
+    "revision": "6087a4641ccdeec276dc",
+    "url": "/web-interface.github.io/static/js/main.67657239.chunk.js"
   },
   {
     "revision": "4a3e44f17b1244e7f39d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web-interface.github.io/precache-manifest.4eca6fbfb4a41cb4ad647b8b150c4939.js"
+  "/web-interface.github.io/precache-manifest.0749baff1c6a9a7e9f33c475058c3726.js"
 );
 
 self.addEventListener('message', (event) => {
