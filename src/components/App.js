b6336e5aee151c9bc7c65ddf9ee6ed58da2314af
Tommus1997 <tom.griebe@gmail.com>
2020-05-30 11:12:58 +0200

    show loading indicator

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Snackbar, Fab } from "@material-ui/core";
+import { Box, Snackbar, Fab, CircularProgress } from "@material-ui/core";
 import { Save, Add } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
 import { ProductAPI } from "../api/ProductAPI";
@@ -21,11 +21,16 @@ const useStyles = makeStyles((theme) => ({
     bottom: theme.spacing(4),
     right: theme.spacing(4),
   },
-
   addButton: {
     position: "fixed",
     bottom: theme.spacing(4) + 64,
     right: theme.spacing(4),
+  },
+  centered: {
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -50%)",
   },
 }));
 
@@ -35,9 +40,12 @@ export const App = () => {
   const [products, setProducts] = useState([]);
   const [successIsOpen, successSetOpen] = useState(false);
   const [failIsOpen, failSetOpen] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    ProductAPI.getProducts().then((res) => setProducts(res));
+    ProductAPI.getProducts()
+      .then((res) => setProducts(res))
+      .finally(() => setLoading(false));
   }, [setProducts]);
 
   const handleSaveButton = () => {
@@ -50,50 +58,53 @@ export const App = () => {
   const handleAddButton = () => {
     setProducts([...products, { id: uuidv4() }]);
   };
-
-  return (
-    <Box className={classes.box}>
-      <ProductTable products={products} setProducts={setProducts} />
-      <Snackbar
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "left",
-        }}
-        open={successIsOpen}
-        autoHideDuration={6000}
-        onClose={() => successSetOpen(false)}
-      >
-        <Alert onClose={() => successSetOpen(false)} severity="success">
-          Successfully Saved!
-        </Alert>
-      </Snackbar>
-      <Snackbar
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "left",
-        }}
-        open={failIsOpen}
-        autoHideDuration={6000}
-        onClose={() => failSetOpen(false)}
-      >
-        <Alert onClose={() => failSetOpen(false)} severity="error">
-          Saving failed!
-        </Alert>
-      </Snackbar>
-      <Fab
-        className={classes.addButton}
-        color={"primary"}
-        onClick={handleAddButton}
-      >
-        <Add />
-      </Fab>
-      <Fab
-        className={classes.saveButton}
-        color={"primary"}
-        onClick={handleSaveButton}
-      >
-        <Save />
-      </Fab>
-    </Box>
-  );
+  if (isLoading) {
+    return <CircularProgress className={classes.centered} />;
+  } else {
+    return (
+      <Box className={classes.box}>
+        <ProductTable products={products} setProducts={setProducts} />
+        <Snackbar
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          open={successIsOpen}
+          autoHideDuration={6000}
+          onClose={() => successSetOpen(false)}
+        >
+          <Alert onClose={() => successSetOpen(false)} severity="success">
+            Successfully Saved!
+          </Alert>
+        </Snackbar>
+        <Snackbar
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          open={failIsOpen}
+          autoHideDuration={6000}
+          onClose={() => failSetOpen(false)}
+        >
+          <Alert onClose={() => failSetOpen(false)} severity="error">
+            Saving failed!
+          </Alert>
+        </Snackbar>
+        <Fab
+          className={classes.addButton}
+          color={"primary"}
+          onClick={handleAddButton}
+        >
+          <Add />
+        </Fab>
+        <Fab
+          className={classes.saveButton}
+          color={"primary"}
+          onClick={handleSaveButton}
+        >
+          <Save />
+        </Fab>
+      </Box>
+    );
+  }
 };
